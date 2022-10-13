e7e92b487cf0f07a6dfb304a5ac83efcf9ace3ef
Oh-byeongju <dhqudwn0@naver.com>
2022-10-13 20:35:25 +0900

    upload

--- _11286.java
+++ _11286.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
-public class Main {
+public class _11286 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
