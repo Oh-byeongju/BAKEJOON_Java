3cfc46d834e4a0b7e6498a964417c20e09a3daf9
Oh-byeongju <dhqudwn0@naver.com>
2022-10-25 18:31:22 +0900

    upload

--- _1475.java
+++ _1475.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class _1475 {
 
     public static void main(String[] args) throws IOException {
 
