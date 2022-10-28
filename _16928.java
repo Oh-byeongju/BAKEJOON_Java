2e34af28505836699cd25eab5fecb73cac822711
Oh-byeongju <dhqudwn0@naver.com>
2022-10-28 20:31:40 +0900

    upload

--- _16928.java
+++ _16928.java
@@ -6,8 +6,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-
-public class Main {
+public class _16928 {
 
     public static int [] graph;
     public static int N, M;
