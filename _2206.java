cf78e47c61422bf75d8bbc5fa4e435d2bc6d267a
Oh-byeongju <dhqudwn0@naver.com>
2022-11-02 23:27:44 +0900

    upload

--- _2206.java
+++ _2206.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class _2206 {
 
     public static int N, M, minVal;
     public static int [][] graph;
