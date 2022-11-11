28b12efd0c8f592106d46a79217c77f026cea468
Oh-byeongju <dhqudwn0@naver.com>
2022-11-12 01:38:56 +0900

    upload

--- Main.java
+++ Main.java
@@ -1,34 +1,14 @@
-//22-11-09
+//22-11-11
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+import static java.lang.System.exit;
+
 public class Main {
 
     public static void main(String [] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        int start = -1;
-        int end = 100001;
-
-        for(int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int S = Integer.parseInt(st.nextToken());
-            int E = Integer.parseInt(st.nextToken());
-
-            if(S > start) {
-                start = S;
-            }
-
-            if(E < end) {
-                end = E;
-            }
-        }
-
-        int result = start - end;
-        System.out.println(Math.max(result, 0));
-
+        System.out.println("문제의 정답");
     }
 }
