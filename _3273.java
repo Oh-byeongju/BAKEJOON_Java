d0cea67e7ca54193546c0ea911e5407d35e6aee0
Oh-byeongju <dhqudwn0@naver.com>
2022-12-15 21:33:04 +0900

    upload

--- _3273.java
+++ _3273.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class _3273 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
