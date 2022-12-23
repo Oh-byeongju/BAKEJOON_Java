08922c3a5c0b254d6bff91e990dcce3d55dc0228
Oh-byeongju <dhqudwn0@naver.com>
2022-12-24 02:49:14 +0900

    upload

--- _9205_bfs.java
+++ _9205_bfs.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class _9205_bfs {
 
     public static int [] dx;
     public static int [] dy;
