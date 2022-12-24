e814213fe947106e7cf26eede244dd6247c23d7c
Oh-byeongju <dhqudwn0@naver.com>
2022-12-24 18:48:46 +0900

    upload

--- _1495_dp.java
+++ _1495_dp.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main {
+public class _1495_dp {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
