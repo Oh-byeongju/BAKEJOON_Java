30807aac4369a86a73997e4e971d8fd6411fbe09
Oh-byeongju <dhqudwn0@naver.com>
2023-05-22 02:20:48 +0900

    DP

--- Main.java
+++ Main.java
@@ -1,68 +1,43 @@
-// 23-05-19 투포인터 문제
+// 23-05-22 DP 문제
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+   public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int [] arr = new int[N];
+       int N = Integer.parseInt(br.readLine());
+       // 0이 작은점프 1이 큰점프
+       int [][] arr = new int[N][2];
+       int [][] dp = new int[N][2];
 
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+       for (int i = 0; i < N - 1; i++) {
+           StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+           for (int j = 0; j < 2; j++) {
+               arr[i][j] = Integer.parseInt(st.nextToken());
+           }
+       }
+       int K = Integer.parseInt(br.readLine());
 
-        int L = 0;
-        int R = 0;
-        int odd = 0;
-        int even = 0;
+       // dp 배열 초기화
+       for (int i = 1; i < N; i++) {
+           for (int j = 0; j < 2; j++) {
+               dp[i][j] = Integer.MAX_VALUE;
+           }
+       }
 
-        // 초기값 설정
-        if (arr[0] % 2 == 0) {
-            even++;
-        }
-        else {
-            odd++;
-        }
-        int max_num = even;
+       // 반복문 시작
+       for (int i = 0; i < N - 1; i++) {
+           for (int j = 0; j < 2; j++) {
 
-        while(R >= L) {
-            // 홀수 제거 가능수가 현재 누적 홀수보다 크거나 같을경우(Right를 넘길 수 있는경우)
-            if (odd <= K) {
-                // Right를 증가시키고 배열을 넘어갈경우 break
-                R++;
-                if (R >= N) {
-                    break;
-                }
-
-                // 현재 배열의 홀짝에 따라 수를 증가
-                if (arr[R] % 2 == 0) {
-                    even++;
-                }
-                else {
-                    odd++;
-                }
-                // 최대수 갱신
-                max_num = Math.max(max_num, even);
-            }
-            // 현재 홀수 누적수가 더 클경우
-            else {
-                // 홀짝에 따라 수를 빼고 Left를 한칸 이동
-                if (arr[L] % 2 == 0){
-                    even--;
-                }
-                else {
-                    odd--;
-                }
-                L++;
-            }
-        }
-        System.out.println(max_num);
-    }
+               // 현재수가 한칸 이동 가능할경우
+               if (i + 1 < N) {
+                   // 큰점프 사용 안한구간
+                   dp[i+1][0] = Math.min(dp[i+1][0], dp[i][0] + arr[i][0]);
+               }
+           }
+       }
+   }
 }
